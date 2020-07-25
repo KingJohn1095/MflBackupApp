3daeee04b8707828536f944106d3601d18f02286
John King <john.king@dmcinfo.com>
2020-07-25 19:31:52 -0400

    Add babel config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,33 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-   entry: './src/index.tsx',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
- plugins: [
-  new CleanWebpackPlugin(),
-   new HtmlWebpackPlugin({
-    title: 'Output Management',
-    template: 'index.html'
-   })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	entry: "./src/index.tsx",
+	devtool: "inline-source-map",
+	devServer: {
+		contentBase: "./dist",
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: "Output Management",
+			template: "index.html",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "babel-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
 };
