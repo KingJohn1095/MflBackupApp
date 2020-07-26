745b5ee97ce31110bf558a035ee97c52452fea27
John King <john.king@dmcinfo.com>
2020-07-26 17:00:45 -0400

    Add comment.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { RosterList } from "./components/RosterList";
+// https://babeljs.io/docs/en/babel-polyfill
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 ReactDOM.render(<RosterList />, document.getElementById("root"));
