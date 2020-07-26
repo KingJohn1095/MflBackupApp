b8fbe80352cfbf37c0775686f7c5296a9628562b
John King <john.king@dmcinfo.com>
2020-07-26 17:00:27 -0400

    Fix regenerator runtime issue.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { RosterList } from "./components/RosterList";
-
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 ReactDOM.render(<RosterList />, document.getElementById("root"));
